28cc52ac7ed15396d2409e86c010ec02bc61de4f
mathias <mathiasgheno@gmail.com>
2024-06-25 01:17:01 -0300

    feat: add npx support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+#!/usr/bin/env node
+
 import chalk from 'chalk';
 import { generatePassword } from './generatePassword.js';
 
-console.log(`${chalk.red.bold('WIP:')} This project is ${chalk.italic.red('Working In Progress')}.`);
+export function init() {
+  console.log(`${chalk.red.bold('WIP:')} This project is ${chalk.italic.red('Working In Progress')}.`);
 
-console.log(chalk.green("################"));
+  console.log(chalk.green("################"));
 
-const types = ['strong', 'medium', 'weak'];
+  const types = ['strong', 'medium', 'weak'];
 
-console.table(
-  types
-    .map(type => ({
-      type,
-      password: generatePassword(type),
-    })),
-);
+  console.table(
+    types
+      .map(type => ({
+        type,
+        password: generatePassword(type),
+      })),
+  );
+}
+
+init();
 
